07b63181b1601be61716eebe0a22536ad84bbf96
Viktoriiapon <viktoriyaponom@gmail.com>
2024-02-10 20:14:00 +0100

    add html start js

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,24 +1,37 @@
 import iziToast from 'izitoast';
-import "izitoast/dist/css/iziToast.min.css";
+import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
-import "simplelightbox/dist/simple-lightbox.min.css";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-
-const ref = {
+const refs = {
   formEl: document.querySelector('.js-search-form'),
   infoEl: document.querySelector('.list-images'),
 };
 
-function getImages(nameImages) {
-  const BASE_URL = 'https://pixabay.com';
-  const END_POINT = '/api/';
-  const PARAMS = `?q=${nameImages}`;
+refs.formEl.addEventListener('submit', onFormSubmit);
 
-  const url = BASE_URL + END_POINT + PARAMS;
+function onFormSubmit(e) {
+  e.preventDefault();
+  const userQuery = e.target.elements.query.value;
 
-  console.log(PARAMS);
-  fetch(url);
+  getImages(userQuery).then(data=>{
+    console.log(data);
+  });
 }
 
-// Виклик функції getImages з певним ім'ям зображень
-getImages('exampleImageName');
+function getImages(nameImages) {
+  const BASE_URL = 'https://pixabay.com/';
+  const END_POINT = '/api/';
+  const PARAMS = `?key=42187150-1e170edc08d41224404163b7f&q=${nameImages}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+  const url = BASE_URL + END_POINT + PARAMS;
+    
+  return fetch(url)
+    .then(res => {
+      return res.json();
+    });
+}
+
+function imageTemplate(nameImages){
+    return ''
+}
