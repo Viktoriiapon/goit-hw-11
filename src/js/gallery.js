6462f235edb14807b5e9bd0737290e07ddf83531
Viktoriiapon <viktoriyaponom@gmail.com>
2024-02-11 17:55:52 +0100

    before combination

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -5,33 +5,87 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
   formEl: document.querySelector('.js-search-form'),
-  infoEl: document.querySelector('.list-images'),
+  infoEl: document.querySelector('.js-list-img'),
+  loaderEl: document.querySelector('.loader'),
 };
 
 refs.formEl.addEventListener('submit', onFormSubmit);
 
+
 function onFormSubmit(e) {
   e.preventDefault();
   const userQuery = e.target.elements.query.value;
+  if (!userQuery) {
+    iziToast.warning({
+      position: 'topRight',
+      message: 'Please enter a search query.',
+    });
+    return;
+  }
 
-  getImages(userQuery).then(data=>{
-    console.log(data);
+  getImage(userQuery).then(data => {
+   renderGallery(data);
   });
+  
 }
 
-function getImages(nameImages) {
+function getImage(nameImage) {
   const BASE_URL = 'https://pixabay.com/';
   const END_POINT = '/api/';
-  const PARAMS = `?key=42187150-1e170edc08d41224404163b7f&q=${nameImages}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const PARAMS = `?key=42187150-1e170edc08d41224404163b7f&q=${nameImage}&image_type=photo&orientation=horizontal&safesearch=true`;
 
   const url = BASE_URL + END_POINT + PARAMS;
-    
-  return fetch(url)
-    .then(res => {
-      return res.json();
-    });
+
+  return fetch(url).then(res => res.json());
+  };
+
+
+function imageTemplate(nameImage) {
+  const {
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads,
+  } = nameImage;
+  return `<div class="image js-image">
+    <div class="image-container">
+
+    <a class="gallery-link" href="${largeImageURL}">
+    <img
+    src="${webformatURL}"
+    alt="${tags}"
+    class="image js-image"
+  />
+  </a>
+    </div>
+    <div class="image-body">
+      <ul class="info">
+    <li class ="info-item">
+      <b class="info-item-title">Likes</b>
+          <span class="info-item-value">${likes}</span>
+          </li>
+          <li class ="info-item">
+      <b class="info-item-title">Views</b>
+          <span class="info-item-value">${views}</span>
+          </li>
+          <li class ="info-item">
+      <b class="info-item-title">Comments</b>
+          <span class="info-item-value">${comments}</span>
+          </li>
+          <li class ="info-item">
+      <b class="info-item-title">Downloads</b>
+          <span class="info-item-value">${downloads}</span>
+          </li>
+      </ul>
+    </div>
+  </div>`;
 }
 
-function imageTemplate(nameImages){
-    return ''
+function renderGallery({ hits }) {
+  const markup = hits.map(imageTemplate).join('');
+  refs.infoEl.insertAdjacentHTML('beforeend', markup);
+  console.log(markup);
 }
